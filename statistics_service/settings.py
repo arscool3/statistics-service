3302206de5465443334a485b04858fabee569c20
arscool3 <45947743+arscool3@users.noreply.github.com>
2021-12-14 17:05:12 +0600

    inital

--- statistics_service/settings.py
+++ statistics_service/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,9 +125,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+
+django_on_heroku.settings(locals())
